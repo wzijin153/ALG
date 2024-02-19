5158a785c9558a568c1b84bf5c50d3c559a861f5
wangzijin <zijin.wang@dolphindb.com>
2024-02-19 18:03:39 +0800

    update

---
+++ Java_ALG/src/Array/BinSearch.java
@@ -0,0 +1,38 @@
+package Array;
+
+// 二分查找 https://leetcode.cn/problems/binary-search/description/
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1
+
+//输入: nums = [-1,0,3,5,9,12], target = 9
+//输出: 4
+//解释: 9 出现在 nums 中并且下标为 4
+
+
+class BinSearch {
+    public int search(int[] nums, int target){
+        if (target < nums[0] || target > nums[nums.length - 1]){
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right){
+            int middle = (left + right) / 2;
+            if (nums[middle] < target){
+                left = middle + 1;
+            }else if (nums[middle] > target){
+                right = middle - 1;
+            }else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        BinSearch binSearch = new BinSearch();
+        int res = binSearch.search(nums, target);
+        System.out.println(res);
+    }
+}
---
+++ Java_ALG/src/Array/RemoveElement.java
@@ -0,0 +1,26 @@
+package Array;
+
+// 移除元素 https://leetcode.cn/problems/remove-element/description/
+// 一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度
+// 示例 1: 给定 nums = [3,2,2,3], val = 3, 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。 你不需要考虑数组中超出新长度后面的元素
+// 示例 2: 给定 nums = [0,1,2,2,3,0,4,2], val = 2, 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.length; fastIndex++){
+            if (nums[fastIndex] != val){
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        return slowIndex;
+    }
+
+    public static void main(String[] args) {
+//        int [] nums = {3, 2, 2, 3};
+        int [] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        RemoveElement test = new RemoveElement();
+        int res = test.removeElement(nums, val);
+        System.out.println(res);
+    }
+}
