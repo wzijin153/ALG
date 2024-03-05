84a27a0f2193d2f807dae75882383523b30509b6
wangzijin <zijin.wang@dolphindb.com>
2024-03-05 10:41:31 +0800

    update SortedSquares

--- Java_ALG/src/Array/SortedSquares.java
+++ Java_ALG/src/Array/SortedSquares.java
@@ -9,9 +9,16 @@ import java.util.Arrays;
  * @create: 2024-02-19 20:17
  **/
 
+// 一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序
 
+// 输入：nums = [-4,-1,0,3,10]
+// 输出：[0,1,9,16,100]
+// 解释：平方后，数组变为 [16,1,0,9,100]，排序后，数组变为 [0,1,9,16,100]
 
-public class SortedSquares {
+// 输入：nums = [-7,-3,2,3,11]
+// 输出：[4,9,9,49,121]
+
+class SortedSquares {
     public int[] method(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             nums[i] = nums[i] * nums[i];
