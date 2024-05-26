2adf8252450a0dc055150e70747b6dff66c90082
zijin.wang <zijin.wang@dolphindb.com>
2024-05-26 13:03:54 +0800

    update some

--- Java_ALG/src/StackAndQueue/maxSlidingWindow.java
+++ Java_ALG/src/StackAndQueue/maxSlidingWindow.java
@@ -9,33 +9,36 @@ import java.util.LinkedList;
  * @author: wangzijin
  * @create: 2024-03-23 16:50
  **/
-
+// https://leetcode.cn/problems/sliding-window-maximum/
 public class maxSlidingWindow {
-    public static void main(String[] args) {
-        maxSlidingWindow test = new maxSlidingWindow();
-        int[] nums = {9, 10, 9, -7, -4, -8, 2, -6};
-        int k = 5;
-        test.method(nums, k);
-    }
+    Deque<Integer> deque = new LinkedList<>();
 
     public int[] method(int[] nums, int k) {
+        // 维护单调递减队列出口处的最大值
         if (nums.length == 1) return nums;
         int[] res = new int[nums.length - k + 1];
-        Deque<Integer> deque = new LinkedList<>();
+
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < deque.getLast()) deque.addLast(nums[i]);
 
         }
-        // 超时
-//        int[] res = new int[nums.length - k + 1];
-//        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-//        for (int i = 0; i <= nums.length - k; i++) {
-//            for (int j = i; j < i + k; j++) {
-//                queue.offer(nums[j]);
-//            }
-//            res[i] = queue.poll();
-//            // 移除窗口最左侧的元素
-//            queue.clear();
-//        }
         return res;
+    }
+
+    void pop(int val) {
+        if (!deque.isEmpty() && val == deque.getFirst()) {
+            deque.pollFirst();
+        }
+    }
+
+    void push(int val) {
+        while (!deque.isEmpty() && val > deque.getLast()) {
+            deque.pollFirst();
+        }
+        deque.addLast(val);
+    }
+
+    int getMaxValue() {
+        return deque.getFirst();
     }
 }
