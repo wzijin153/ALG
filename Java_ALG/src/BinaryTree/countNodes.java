f18b2078169b0a34d3c16255af84c2d394c69c5e
zijin.wang <zijin.wang@dolphindb.com>
2024-05-29 22:40:59 +0800

    update some

--- Java_ALG/src/BinaryTree/countNodes.java
+++ Java_ALG/src/BinaryTree/countNodes.java
@@ -12,7 +12,11 @@ import java.util.Queue;
 // https://leetcode.cn/problems/count-complete-tree-nodes/
 public class countNodes {
     public int method(TreeNode root) {
-        // 1.迭代法
+        /**
+         * 1.迭代法
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(n)
+         */
         int count = 0;
         if (root == null) return count;
         Queue<TreeNode> queue = new LinkedList<>();
@@ -37,12 +41,43 @@ public class countNodes {
         return count;
     }
 
-    // 2.递归法
+    /**
+     * 2.递归法
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(log n), 算上了递归系统栈占用的空间
+     */
     int getCount(TreeNode node) {
         if (node == null) return 0;
         int leftCount = getCount(node.left);
         int rightCount = getCount(node.right);
         // 加1是因为算上当前中间节点
         return leftCount + rightCount + 1;
+    }
+
+    /**
+     * 3.利用完全二叉树和满二叉树的性质
+     * 时间复杂度：O(log n * log n)
+     * 空间复杂度：O(log n)
+     */
+    int getNum(TreeNode node) {
+        if (node == null) return 0;
+        // 开始根据左深度和右深度是否相同来判断该子树是不是满二叉树
+        TreeNode left = node.left, right = node.right;
+        int leftDepth = 0, rightDepth = 0;
+        // 求左子树深度
+        while (left != null) {
+            left = left.left;
+            leftDepth++;
+        }
+        // 求右子树深度
+        while (right != null) {
+            right = right.right;
+            rightDepth++;
+        }
+        // 满二叉树的结点数为：2^depth - 1
+        if (leftDepth == rightDepth) return (2 << leftDepth) - 1;
+        int leftNum = getNum(node.left);
+        int rightNum = getNum(node.right);
+        return leftNum + rightNum + 1;
     }
 }
